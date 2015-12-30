fd766a78b163ffeae09c43f483df9db1be33cee6
ebaumeis <ebaumeis@umich.edu>
2015-12-29 22:00:25 -0600

    Slightly modified card model

--- models.py
+++ models.py
@@ -5,6 +5,11 @@ from cardbox import db
 class Card(db.Document):
   created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
   name = db.StringField()
+  card_id = db.StringField()
+  types = db.StringField()
+  colors = db.StringField()
+  cost = db.StringField()
+  text = db.StringField()
 
   def display(self):
-  	return '{}'.format(self.name)
+  	return '{} {}'.format(self.name, self.card_id)
