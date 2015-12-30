2eb47cc40d0230e35878be79793b3646a5dcb876
ebaumeis <ebaumeis@umich.edu>
2015-12-29 22:14:21 -0600

    Updated card model to work with first page of deckbrew cards

--- models.py
+++ models.py
@@ -6,10 +6,22 @@ class Card(db.Document):
   created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
   name = db.StringField()
   card_id = db.StringField()
+  url = db.StringField()
+  store_url = db.StringField()
   types = db.StringField()
+  subtypes = db.StringField()
   colors = db.StringField()
+  cmc = db.IntField()
   cost = db.StringField()
   text = db.StringField()
+  power = db.IntField()
+  toughness = db.IntField()
+  formats = db.StringField()
+  editions = db.StringField()
+  loyalty = db.StringField()
+  supertypes = db.StringField()
 
   def display(self):
-  	return '{} {}'.format(self.name, self.card_id)
+  	return '{} {}/{}'.format(self.name, self.power, self.toughness)
+
+
